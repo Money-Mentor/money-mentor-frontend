1b487718e3486a32fc26c746aaac3e00ad564f74
Sharon Yun <sharonyun11@gmail.com>
2018-06-29 12:47:38 -0400

    currSpent per category done

--- client/screens/Home/StackedBar.js
+++ client/screens/Home/StackedBar.js
@@ -6,66 +6,66 @@ class StackedBar extends React.Component {
   constructor(props) {
     super(props);
     this.getValueFromCategory = this.getValueFromCategory.bind(this);
+    this.getTotalAllowed = this.getTotalAllowed.bind(this);
   }
 
   getValueFromCategory(str) {
-    const catValue = this.props.spendingByCategory.map(elem => {
-      if (elem.name === str) {
-        return elem.value;
-      }
-    });
-    return catValue;
+    const categories = this.props.spendingByCategory[0];
+    const currCategory =
+      categories && categories.filter(elem => elem.name === str)[0];
+    return !currCategory ? 0 : currCategory.number;
   }
 
-  render() {
-    console.log('props *********', this.props.spendingByCategory);
-    console.log('GET DATAAAA++++', this.props.getData);
+  getTotalAllowed(str) {}
 
+  render() {
+    console.log('what is data!!!!!!!', this.props.getData);
     const data = [
       {
         category: 'Food and Drink',
-        totalAllowed: this.getValueFromCategory('Food and Drink'),
-        currSpent: 480,
+        totalAllowed: 0,
+        currSpent: this.getValueFromCategory('Food and Drink'),
         allocatedAmountByDate: 640
       },
       {
         category: 'Community',
         totalAllowed: 3320,
-        currSpent: 480,
+        currSpent: this.getValueFromCategory('Community'),
         allocatedAmountByDate: 640
       },
       {
         category: 'Healthcare',
         totalAllowed: 3320,
-        currSpent: 480,
+        currSpent: this.getValueFromCategory('Healthcare'),
         allocatedAmountByDate: 640
       },
       {
         category: 'Recreation',
         totalAllowed: 3320,
-        currSpent: 480,
+        currSpent: this.getValueFromCategory('Recreation'),
         allocatedAmountByDate: 640
       },
       {
         category: 'Service',
         totalAllowed: 3320,
-        currSpent: 480,
+        currSpent: this.getValueFromCategory('Service'),
         allocatedAmountByDate: 640
       },
       {
         category: 'Shops',
         totalAllowed: 3320,
-        currSpent: 480,
+        currSpent: this.getValueFromCategory('Shops'),
         allocatedAmountByDate: 640
       },
       {
         category: 'Travel',
         totalAllowed: 3320,
-        currSpent: 480,
+        currSpent: this.getValueFromCategory('Travel'),
         allocatedAmountByDate: 640
       }
-    ];
-    console.log('this should be the number ::::::', data[0]);
+    ].filter(elem => elem.currSpent !== 0);
+    console.log('Category Object for FoodDrink', data[0]);
+    console.log('Category Object for Category', data[1]);
 
     const colors = ['#7b4173', '#a55194', '#ce6dbd', '#de9ed6'];
     const keys = [
@@ -76,6 +76,9 @@ class StackedBar extends React.Component {
     ];
 
     return (
+      // <View>
+      //   <Text>Hello</Text>
+      // </View>
       <StackedBarChart
         style={{ height: 200 }}
         keys={keys}
